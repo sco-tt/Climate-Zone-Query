4e0022f6dcfb29c475e54a0bed362cb38da4d281
Scott Pinkelman <scott@scottpinkelman.com>
2016-09-09 10:07:45 -0600

    Working code to get the map to display

--- src/App.js
+++ src/App.js
@@ -1,21 +1,119 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {default as React, Component} from 'react';
+import {default as update} from 'react-addons-update';
+import {default as canUseDOM} from 'can-use-dom';
+import {default as _} from 'lodash';  import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
+import {triggerEvent} from 'react-google-maps/lib/utils';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+
+class Mapping extends Component {
+
+  state = {
+   markers: [{
+     position: {
+       lat: 25.0112183,
+       lng: 121.52067570000001,
+     },
+     key: 'Taiwan',
+     defaultAnimation: 2
+   }]
+  };
+
+  constructor (props, context) {
+   super(props, context);
+   this.handleWindowResize = _.throttle(this.handleWindowResize, 500);
   }
-}
 
-export default App;
+  componentDidMount () {
+   if (!canUseDOM) {
+     return;
+   }
+   window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount () {
+    if (!canUseDOM) {
+      return;
+    }
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize () {
+    console.log('handleWindowResize', this._googleMapComponent);
+    triggerEvent(this._googleMapComponent, 'resize');
+  }
+
+  /**
+   * This is called when you click on the map.
+   * Go and try click now.
+   */
+  handleMapClick (event) {
+    var {markers} = this.state;
+    markers = update(markers, {
+      $push: [
+      {
+        position: event.latLng,
+        defaultAnimation: 2,
+                  key: Date.now() // Add a key property for: http://fb.me/react-warning-keys
+                },
+                ],
+              });
+    this.setState({ markers });
+
+    if (3 === markers.length) {
+      this.props.toast(
+        'Right click on the marker to remove it',
+        'Also check the code!'
+        );
+    }
+  }
+
+  handleMarkerRightclick (index, event) {
+    /*
+     * All you modify is data, and the view is driven by data.
+     * This is so called data-driven-development. (And yes, it's now in
+     * web front end and even with google maps API.)
+     */
+     var {markers} = this.state;
+     markers = update(markers, {
+      $splice: [
+      [index, 1]
+      ],
+    });
+     this.setState({ markers });
+   }
+
+  render () {
+    return (
+     <GoogleMapLoader
+       containerElement={
+        <div
+        {...this.props}
+        style={{
+          height: '400px',
+          width: '400px'
+        }}
+        />
+      }
+      googleMapElement={
+        <GoogleMap
+          ref={(map) => (this._googleMapComponent = map) && console.log(map.getZoom())}
+          defaultZoom={3}
+          defaultCenter={{lat: -25.363882, lng: 131.044922}}
+          onClick={this.handleMapClick}>
+        
+          {this.state.markers.map((marker, index) => {
+            return (
+              <Marker
+              {...marker}
+              onRightclick={this.handleMarkerRightclick.bind(this, index)} />
+              );
+          })}
+        </GoogleMap>
+      } // googleMapElement
+    /> // GoogleMapLoad
+    ); // return
+  } // render
+} //
+
+export default Mapping;
