729cfca549d5c84d70a9a9903cb0c7a2cd42bee3
Scott Pinkelman <scott@scottpinkelman.com>
2016-09-13 22:48:28 -0600

    Adds basic rounding method to ClimateZoneLookup

--- src/ClimateZoneLookup.js
+++ src/ClimateZoneLookup.js
@@ -1,16 +1,58 @@
 import {default as React, Component} from 'react';
+import axios from 'axios';
+
 
 class ClimateZoneLookup extends Component {
    static style = {
     'color': `red`
   }
   componentDidUpdate() {
-    console.log('componentDidUpdate');
     this.geocode();
   }
   geocode() {
     console.log('geocode');
-    console.log(this.props);
+    const queryAddress = this.props.address
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${queryAddress}&key=AIzaSyB8eYNd6em4gm8uLs8updwtSMnwR5X-EQQ`)
+      .then((response) => {
+        console.log(response);
+        const lat = response.data.results[0].geometry.location.lat;
+        const lng = response.data.results[0].geometry.location.lng;
+        const postion = [lat, lng];
+        this.round(postion);
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  round(postion) {
+      postion.forEach((coord) => {
+        console.log(coord);
+        let decimal = (coord % 1);
+          if (decimal >= 0 && decimal <= 0.5) {
+              coord += (0.25 - decimal);
+          }
+           if (decimal > 0.5 && decimal < 1) {
+              coord += (0.75 - decimal);
+          }
+          if (decimal >= -0.5 && decimal  < 0) {
+               coord += (-0.25 - decimal);
+           }
+          if (decimal > -1.00 && decimal < -0.5) {
+              coord += (-0.75 - decimal);
+         }
+         postion.push(coord);
+      });
+      console.log(postion);
+        
+  }
+
+  lookup(coord) {
+    const query = "SELECT 'Cls' FROM " +
+                 "1GQfBT-PXojUbIZP7_tkILYKNjHaQjYqop9gkosho" +
+                 " WHERE 'Lat' = '" + lat + "' AND Lon = '" + lng + "'";
+
   }
 
   render() {
