2ef0695c25b8df1d32f1592b383a74dcd49cb814
Scott Pinkelman <scott@scottpinkelman.com>
2016-11-15 20:05:04 -0700

    Sucessfully implements chcheckbox togle using ToggleOverlay state

--- src/ToggleOverlay.js
+++ src/ToggleOverlay.js
@@ -1,42 +1,29 @@
 import {default as React, Component} from 'react';
 
 class ToggleOverlay extends Component {
-
+  
   constructor(props) {
     super(props);
-    this.state = {checked: false};
-    this._handleCheckboxChange = this._handleCheckboxChange.bind(this);
-  }
-
-  componentWillReceiveProps() {
-    this.setState({
-      preserveViewport: true
-    })
+    this.state = {isChecked: false};
+    this.onChange = this.onChange.bind(this);
   }
   
-  _handleCheckboxChange(event) {
-    event.preventDefault()
-    this.props.setOverlay(); 
-    
-    this.setState({
-      checked: !this.state.checked
-    })
+  onChange() {
+    this.setState({isChecked: !this.state.isChecked});
+    this.props.setOverlay();
   }
 
   render() {
     return (
-      // <button onClick={this._handleClick.bind(this)} className={"toggle-button"}>Toggle Overlay</button>
-      <div>
-        <input 
-        type="checkbox" 
-        label="Toggle Climate Zone Overlay" 
-        id="toggle-checkbox"
-        checked={this.state.checked}
-        onChange={this._handleCheckboxChange}
+      <label>
+        <input
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.onChange}
         />
-        <div>Toggle Climate Zone overlay</div>
-      </div>
-    );    
+        Toggle Overlay
+      </label>
+    );
   }
 }
 
